55bf88f77f8d1562a3e543131ccfe76392939923
Tannu <8901tannu@gmail.com>
2025-06-23 19:50:53 +0530

    Remove file with credentials

--- FrontEnd/htdocs/dbconnection.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-try {
-    $mongoClient = new MongoDB\Driver\Manager("mongodb+srv://8901tannu:yP34UMeSn7LIJnYc@cluster0.mwqyc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");  
-    echo "Connected to MongoDB successfully!";
-} catch (Exception $e) {
-    die("Connection failed: " . $e->getMessage());
-}
-?>
